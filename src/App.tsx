39aa034a995afe8a8980022422796e9b43c4276e
Mike Coomber <mike.coomber@thegymgroup.com>
2025-03-20 14:46:27 +0000

    refactor logic into usePages

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { Fonts } from "./theme/fonts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Pages from "./components/Pages/Pages";
 import { usePages } from "./data/usePages";
-import { useStore } from "@tanstack/react-store";
-import { PageStore, pageStore } from "./data/store";
 
 const queryClient = new QueryClient();
 
@@ -27,24 +25,9 @@ export default App;
 
 function Main() {
   const { data, error, isPending } = usePages();
-  const store = useStore(pageStore);
 
-  // If the api is successful, render the pages and update the store
   if (data) {
-    pageStore.setState(() => {
-      const pageMap = data.reduce((acc: PageStore, page) => {
-        acc[page.id] = page;
-        return acc;
-      }, {});
-      return pageMap;
-    });
     return <Pages pages={data} />;
-  }
-
-  // If the request is loading or unsuccessful and we have data in the store, render the stored pages
-  if (store) {
-    const pagesArray = Object.values(store);
-    return <Pages pages={pagesArray} />;
   }
 
   if (isPending) {
--- src/data/usePages.ts
+++ src/data/usePages.ts
@@ -1,9 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { getConfig } from "../api/get-config";
+import { PageStore, pageStore } from "./store";
+import { useStore } from "@tanstack/react-store";
 
 export function usePages() {
-  return useQuery({
+  const { data, isPending, error } = useQuery({
     queryKey: ["config"],
     queryFn: getConfig,
   });
+  const store = useStore(pageStore);
+
+  // If the api is successful, update the store
+  // TODO: remove react-store, implement local file storage
+  if (data) {
+    pageStore.setState(() => {
+      const pageMap = data.reduce((acc: PageStore, page) => {
+        acc[page.id] = page;
+        return acc;
+      }, {});
+      return pageMap;
+    });
+  }
+
+  if (error) {
+    console.error(error);
+  }
+
+  return {
+    isPending,
+    data: data ?? (store ? Object.values(store) : undefined),
+    error,
+  };
 }

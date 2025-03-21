cc77ae79febbd991b369af317d94fd8b5baaa27e
Mike Coomber <mike.coomber@thegymgroup.com>
2025-03-21 16:17:23 +0000

    rename

--- src/api/get-pages.ts
+++ src/api/get-pages.ts
@@ -1,7 +1,7 @@
 import { pages } from "../data/chart_data";
 import { PageConfig } from "../types";
 
-export async function getConfig(): Promise<PageConfig[]> {
+export async function getPages(): Promise<PageConfig[]> {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(pages);

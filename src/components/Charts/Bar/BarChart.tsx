305a68bc3a1dd7776c5c8cec45de5c95af7d5d51
yasingulamhussain <yasin.gulamhussain@thegymgroup.com>
2025-03-27 13:28:05 +0000

    truncated bar chart x axis to avoid blank space

--- src/components/Charts/Bar/BarChart.tsx
+++ src/components/Charts/Bar/BarChart.tsx
@@ -12,13 +12,17 @@ export function BarChart({ data, colour: color, animate }: ChartProperties) {
   return (
     <ChartContainer width="100%" height="100%">
       <RechartBarChart {...baseChartProps} data={data}>
-        <XAxis dataKey="label" />
+        <XAxis 
+        dataKey="label"
+          tickFormatter={(value) =>
+            value.length > 10 ? `${value.slice(0, 10)}...` : value}
+        />
         <Bar
           {...chartAnimationProps}
           isAnimationActive={animate}
           dataKey="value"
           fill={color?.hex ?? chartColours[0].hex}
-          label={{ position: "top", fontSize: 32 }}
+          label={{ position: "top", fontSize: 32}}
         />
       </RechartBarChart>
     </ChartContainer>
